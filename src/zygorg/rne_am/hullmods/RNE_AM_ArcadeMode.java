f78de562dda8906faa7708cadc53676834cbf7bc
OfficialZygorg <43007952+OfficialZygorg@users.noreply.github.com>
2026-09-13 14:42:15 +0200

    Trying to fix a CTD based on a null value Added debug logging

--- src/zygorg/rne_am/hullmods/RNE_AM_ArcadeMode.java
+++ src/zygorg/rne_am/hullmods/RNE_AM_ArcadeMode.java
@@ -79,6 +79,7 @@ public class RNE_AM_ArcadeMode extends BaseHullMod implements LunaSettingsListen
   }
   private void modifyStat(MutableStat stat, String id, float value, String type) {
     log.log(Level.INFO, "(DEBUG) value:" + value + " type: " + type);
+    if (type == null) return;
     switch (type) {
       case "Add":
         stat.modifyFlat(id, value);
